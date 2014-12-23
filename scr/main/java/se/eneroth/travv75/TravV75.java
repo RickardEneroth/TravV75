2b50305e205e204a5c70c6ee11d30e6c07813879
Rickard <rickard_eneroth@hotmail.com>
2014-12-23 14:53:57 +0100

    First

---
+++ scr/main/java/se/eneroth/travv75/TravV75.java
@@ -0,0 +1,10 @@
+package se.eneroth.travv75;
+
+/**
+ * Created by Rickard on 2014-12-23.
+ */
+public class TravV75 {
+    public static void main(String args[]) {
+        System.out.println("Hello V75!");
+    }
+}
